cf927752215cd888dba34d90686546258a6d6f80
Josh Sommer <JoshSo@Joshs-MacBook-Pro.local>
2018-12-09 21:57:27 -0500

    example usage in home component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,19 +1,17 @@
-import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
-import { NativeScriptCommonModule } from "nativescript-angular/common";
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { NativeScriptCommonModule } from 'nativescript-angular/common';
 
-import { HomeRoutingModule } from "./home-routing.module";
-import { HomeComponent } from "./home.component";
+import { CurrentConnectionModule } from 'nativescript-ngx-swiss-army-knife';
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
 
 @NgModule({
-    imports: [
-        NativeScriptCommonModule,
-        HomeRoutingModule
-    ],
-    declarations: [
-        HomeComponent
-    ],
-    schemas: [
-        NO_ERRORS_SCHEMA
-    ]
+  imports: [
+    NativeScriptCommonModule,
+    HomeRoutingModule,
+    CurrentConnectionModule.forRoot()
+  ],
+  declarations: [HomeComponent],
+  schemas: [NO_ERRORS_SCHEMA]
 })
-export class HomeModule { }
+export class HomeModule {}
